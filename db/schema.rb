4085e0b714275bb39d3eb0aad0d23160853478ee
Alisa Chang <laptite@gmail.com>
2020-03-10 17:09:30 -0700

    Add association between users and docs

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_233314) do
+ActiveRecord::Schema.define(version: 2020_03_10_234307) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
