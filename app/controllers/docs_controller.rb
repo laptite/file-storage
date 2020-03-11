291286b0ed7852f7752ac9d1606841b0ea8b249f
Alisa Chang <laptite@gmail.com>
2020-03-10 21:37:47 -0700

    Add initial styles and welcome page text

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,7 +2,7 @@ class DocsController < ApplicationController
 	before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@docs = Doc.where(user_id: current_user).order("created_at DESC")
+		@docs = Doc.where(user_id: current_user).order(created_at: :desc)
 	end
 
 	def new
